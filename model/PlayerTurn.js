0cf0d8465fba772454474381abd4ceda17c77e9d
Jeffrey M. Thompson <jmthompson2015@users.noreply.github.com>
2020-06-01 13:25:15 -0600

    Refactored PlayerTurn to use reduce with Promises.

--- model/PlayerTurn.js
+++ model/PlayerTurn.js
@@ -6,40 +6,19 @@ import StepFunction from "./StepFunction.js";
 
 const PlayerTurn = {};
 
-const executeSteps = (playerIds, index, store) => {
-  let answer;
-
-  if (playerIds.length > index) {
-    store.dispatch(ActionCreator.setCurrentPlayer(playerIds[index]));
-    answer = StepFunction.execute(store);
-  }
-
-  return answer;
-};
-
-const executePlayerTurns = (store) => {
-  const playerIds = Selector.currentPlayerOrder(store.getState());
-
-  return new Promise((resolve) => {
-    store.dispatch(ActionCreator.setCurrentPlayer(playerIds[0]));
-    StepFunction.execute(store).then(resolve);
-  })
-    .then(() => executeSteps(playerIds, 1, store))
-    .then(() => executeSteps(playerIds, 2, store))
-    .then(() => executeSteps(playerIds, 3, store))
-    .then(() => executeSteps(playerIds, 4, store))
-    .then(() => executeSteps(playerIds, 5, store))
-    .then(() => {
-      store.dispatch(ActionCreator.setCurrentPlayer(undefined));
-    });
-};
-
 PlayerTurn.execute = (store) =>
   new Promise((resolve) => {
     if (GameOver.isGameOver(store)) {
       resolve();
     } else {
-      executePlayerTurns(store).then(() => {
+      const playerIds = Selector.currentPlayerOrder(store.getState());
+      const reduceFunction = (promise, playerId) =>
+        promise.then(() => {
+          store.dispatch(ActionCreator.setCurrentPlayer(playerId));
+          return StepFunction.execute(store);
+        });
+      R.reduce(reduceFunction, Promise.resolve(), playerIds).then(() => {
+        store.dispatch(ActionCreator.setCurrentPlayer(undefined));
         resolve();
       });
     }
