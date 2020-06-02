2966be929347515f6f9cb269e28633b49c987983
Jeffrey M. Thompson <jmthompson2015@users.noreply.github.com>
2020-06-02 13:02:05 -0600

    Refactored PhaseFunction to use reduce with Promises.

--- model/PhaseFunction.js
+++ model/PhaseFunction.js
@@ -1,57 +1,27 @@
-/* eslint no-console: ["error", { allow: ["log"] }] */
-
 import Phase from "../artifact/Phase.js";
 
 import ActionCreator from "../state/ActionCreator.js";
-import Selector from "../state/Selector.js";
 
 import GameOver from "./GameOver.js";
 import PlayerTurn from "./PlayerTurn.js";
 
 const PhaseFunction = {};
 
-const advancePhase = (store) => {
-  const oldPhase = Selector.currentPhase(store.getState());
-  const oldPhaseKey = oldPhase ? oldPhase.key : undefined;
-  const phaseKeys = Phase.keys();
-  let newPhaseKey;
-
-  if (R.isNil(oldPhaseKey)) {
-    [newPhaseKey] = phaseKeys; // first element
-  } else {
-    const index = phaseKeys.indexOf(oldPhaseKey);
-
-    if (index === phaseKeys.length - 1) {
-      // last phase.
-      newPhaseKey = undefined;
-    } else {
-      newPhaseKey = phaseKeys[index + 1];
-    }
-  }
-
-  store.dispatch(ActionCreator.setCurrentPhase(newPhaseKey));
-};
-
-PhaseFunction.executePhases = (resolve, store) => {
-  advancePhase(store);
-  const phase = Selector.currentPhase(store.getState());
-  const phaseKey = phase ? phase.key : undefined;
-
-  if (R.isNil(phaseKey)) {
-    resolve();
-  } else {
-    PlayerTurn.execute(store).then(() => {
-      PhaseFunction.executePhases(resolve, store);
-    });
-  }
-};
-
 PhaseFunction.execute = (store) =>
   new Promise((resolve) => {
     if (GameOver.isGameOver(store)) {
       resolve();
     } else {
-      PhaseFunction.executePhases(resolve, store);
+      const phaseKeys = Phase.keys();
+      const reduceFunction = (promise, phaseKey) =>
+        promise.then(() => {
+          store.dispatch(ActionCreator.setCurrentPhase(phaseKey));
+          return PlayerTurn.execute(store);
+        });
+      R.reduce(reduceFunction, Promise.resolve(), phaseKeys).then(() => {
+        store.dispatch(ActionCreator.setCurrentPhase(undefined));
+        resolve();
+      });
     }
   });
 
